488bae50893ad976ac5fbab14ac11512f229cd1a
Kramer Brown <brownkramer63@gmail.com>
2023-05-02 17:09:55 -0500

    first push duh

---
+++ Test.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("just push")
+message ="meet me tonight. string"
+print(message)
+message2='why is this not in double quotes'
+print(message2)
+#look at git ignore folder its been loading for like an hour i may have an issue with this
+a=999
+type(a) # what does type mean
+a # this and type not showing in terminal hmmm
+print(a)
+z= 2-6.1j
+print(type(z))
+z.real
+z.imag
+
